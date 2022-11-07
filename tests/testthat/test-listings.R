11d0b17525892a35758f18f0ad6b1a0ea6fc85e5
Melkiades <davide.garolini@roche.com>
2022-11-07 17:01:04 +0100

    formatting current tests

--- tests/testthat/test-listings.R
+++ tests/testthat/test-listings.R
@@ -17,7 +17,8 @@ testthat::test_that("Column labels are the same", {
 })
 
 
-testthat::test_that("Error with NA values in numeric column when apply format", {
+testthat::test_that("listings work well with different formats and attributes", {
+  # (1) Error with NA values in numeric column when apply format
   anl_tmp <- anl
   var_labels(anl_tmp) <- var_labels(formatters::ex_adsl)[c("USUBJID", "ARM", "BMRKR1")]
   anl_tmp$BMRKR1[1:3] <- NA
@@ -37,94 +38,95 @@ testthat::test_that("Error with NA values in numeric column when apply format", 
     unname(mat$strings[2, 3, drop = TRUE]),
     "NA"
   )
+
+  ## this tests that the format is applied correctly
+  expect_identical(
+    unname(mat$strings[3, 3, drop = TRUE]),
+    format_value(lsting$BMRKR1[2], "xx.xx")
+  )
+
+  expect_identical(main_title(lsting), "main title")
+  expect_identical(main_title(lsting), main_title(mat))
+
+  expect_identical(subtitles(lsting), c("sub", "titles"))
+  expect_identical(subtitles(lsting), subtitles(mat))
+
+  expect_identical(main_footer(lsting), "main footer")
+  expect_identical(main_footer(lsting), main_footer(mat))
+
+  expect_identical(prov_footer(lsting), "provenance")
+  expect_identical(prov_footer(lsting), prov_footer(mat))
+
+  expect_error(
+    {
+      main_title(lsting) <- 1L
+    },
+    "value for main_title .*class: integer$"
+  )
+
+  expect_error(
+    {
+      main_title(lsting) <- c("lol", "silly")
+    },
+    "value for main_title .*got vector of length 2"
+  )
+
+  expect_error(
+    {
+      subtitles(lsting) <- 1L
+    },
+    "value for subtitles .*class: integer$"
+  )
+
+  expect_error(
+    {
+      main_footer(lsting) <- 1L
+    },
+    "value for main_footer .*class: integer$"
+  )
+
+  expect_error(
+    {
+      prov_footer(lsting) <- 1L
+    },
+    "value for prov_footer .*class: integer$"
+  )
+
+  main_title(lsting) <- NULL
+  expect_identical(main_title(lsting), character())
 })
 
-## this tests that the format is applied correctly
-expect_identical(
-  unname(mat$strings[3, 3, drop = TRUE]),
-  format_value(lsting$BMRKR1[2], "xx.xx")
-)
+testthat::test_that("Content of listings supports newlines", {
+  ## newline support #16
 
-expect_identical(main_title(lsting), "main title")
-expect_identical(main_title(lsting), main_title(mat))
+  mydf <- data.frame(col1 = c(1, 1, 2), col2 = c("hi\nthere", "what", "who"))
+  var_labels(mydf) <- c("column\n1", "column 2")
+  mylst <- as_listing(mydf, names(mydf), key_cols = "col1")
 
-expect_identical(subtitles(lsting), c("sub", "titles"))
-expect_identical(subtitles(lsting), subtitles(mat))
+  mpf <- matrix_form(mylst)
 
-expect_identical(main_footer(lsting), "main footer")
-expect_identical(main_footer(lsting), main_footer(mat))
+  expect_identical(
+    mpf$strings[1:2, 1, drop = TRUE],
+    c("column", "1")
+  )
 
-expect_identical(prov_footer(lsting), "provenance")
-expect_identical(prov_footer(lsting), prov_footer(mat))
+  rdf <- make_row_df(mylst)
 
-expect_error(
-  {
-    main_title(lsting) <- 1L
-  },
-  "value for main_title .*class: integer$"
-)
+  expect_identical(
+    rdf$self_extent,
+    c(2L, 1L, 1L)
+  )
+})
 
-expect_error(
-  {
-    main_title(lsting) <- c("lol", "silly")
-  },
-  "value for main_title .*got vector of length 2"
-)
+testthat::test_that("regression test for keycols being lost due to `head()`", {
+  ## causing #41
+  rlst <- as_listing(mtcars,
+    key_cols = c("gear", "carb"),
+    cols = c("gear", "carb", "qsec")
+  )
 
-expect_error(
-  {
-    subtitles(lsting) <- 1L
-  },
-  "value for subtitles .*class: integer$"
-)
-
-expect_error(
-  {
-    main_footer(lsting) <- 1L
-  },
-  "value for main_footer .*class: integer$"
-)
-
-expect_error(
-  {
-    prov_footer(lsting) <- 1L
-  },
-  "value for prov_footer .*class: integer$"
-)
-
-main_title(lsting) <- NULL
-expect_identical(main_title(lsting), character())
-
-
-## newline support #16
-
-mydf <- data.frame(col1 = c(1, 1, 2), col2 = c("hi\nthere", "what", "who"))
-var_labels(mydf) <- c("column\n1", "column 2")
-mylst <- as_listing(mydf, names(mydf), key_cols = "col1")
-
-mpf <- matrix_form(mylst)
-
-expect_identical(
-  mpf$strings[1:2, 1, drop = TRUE],
-  c("column", "1")
-)
-
-rdf <- make_row_df(mylst)
-
-expect_identical(
-  rdf$self_extent,
-  c(2L, 1L, 1L)
-)
-
-
-## regression test for keycols being lost due to head
-## causing #41
-rlst <- as_listing(mtcars,
-  key_cols = c("gear", "carb"),
-  cols = c("gear", "carb", "qsec")
-)
-
-expect_identical(
-  dim(head(rlst, 5)),
-  c(5L, ncol(mtcars))
-)
+  expect_identical(
+    dim(head(rlst, 5)),
+    c(5L, ncol(mtcars))
+  )
+})
