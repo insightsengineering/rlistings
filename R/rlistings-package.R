464c3ce7955f0cf050f19bdc1ac6a9de4228ad79
Melkiades <davide.garolini@roche.com>
2022-11-10 15:42:39 +0100

    funder?

--- R/rlistings-package.R
+++ R/rlistings-package.R
@@ -1,10 +1,8 @@
 #' @keywords internal
 "_PACKAGE"
 
-## usethis namespace: start
 #' @import formatters
 #' @import tibble
 #' @import methods
 #' @importFrom utils head tail
-## usethis namespace: end
 NULL
