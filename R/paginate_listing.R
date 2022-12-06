5df31531255b812ccd2e427d925bf388d856f5d1
ayogasekaram <abinaya.yogasekaram@contractors.roche.com>
2022-12-06 16:24:49 +0000

    Update paginate_listing function to handle lpp=NULL. Adding unit test for lpp=NULL.

--- R/paginate_listing.R
+++ R/paginate_listing.R
@@ -58,9 +58,17 @@ paginate_listing <- function(lsting, lpp = 15,
   if (flines > 0) {
     flines <- flines + dheight + 1L
   }
-  ## row lines per page
-  rlpp <- lpp - cinfo_lines - tlines - flines
-  pagdf <- make_row_df(lsting, colwidths)
+
+  if (!is.null(lpp)) {
+    ## row lines per page
+    rlpp <- lpp - cinfo_lines - tlines - flines
+
+    pagdf <- make_row_df(lsting, colwidths)
+  } else {
+    pagdf <- make_row_df(lsting, colwidths)
+    rlpp <- nrow(pagdf)
+  }
+
 
   inds <- pag_indices_inner(pagdf,
     rlpp = rlpp,
