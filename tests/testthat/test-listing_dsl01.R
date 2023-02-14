edf707440c65bad6cbfa718c9ef0c34a0b8fca6b
Melkiades <davide.garolini@roche.com>
2023-02-14 11:06:52 +0100

    shallow change # 2

--- tests/testthat/test-listing_dsl01.R
+++ tests/testthat/test-listing_dsl01.R
@@ -43,7 +43,6 @@ testthat::test_that("DSL01 listing is produced correctly", {
     ),
     nrow = 13
   )
-
   testthat::expect_identical(mf_strings(result_matrix), expected_strings)
 
   # Test DSL01 headers/footers
