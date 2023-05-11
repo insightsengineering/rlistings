53a2e1072e03b3ac261ed02799a58ef2127ba7ca
shajoezhu <sha.joe.zhu@gmail.com>
2023-05-11 21:17:11 +0800

    remove files that are no longer needed

--- R/rlistings-package.R
+++ R/rlistings-package.R
@@ -5,8 +5,5 @@
 #' @import tibble
 #' @import methods
 #' @importFrom utils head tail
-#' @importFrom grid textGrob grid.newpage pushViewport plotViewport unit grid.draw
-#' @importFrom grid convertHeight convertWidth get.gpar gpar
-#' @importFrom grDevices dev.off pdf
 #' @importFrom stats na.omit
 NULL
