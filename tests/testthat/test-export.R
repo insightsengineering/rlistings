da655837d3a8bbb4de5a500348f0ee46b111f13b
ayogasekaram <abinaya.yogasekaram@contractors.roche.com>
2023-08-15 13:21:12 +0000

    Updating tests and snapshots.

--- tests/testthat/test-export.R
+++ tests/testthat/test-export.R
@@ -19,10 +19,27 @@ testthat::test_that("key columns repeat with export_as_txt", {
   ) %>%
     add_listing_col("BMRKR1", format = "xx.x")
 
-  listing_exp <- suppressMessages(export_as_txt(lsting, lpp = 4, verbose = TRUE,
+  listing_exp <- suppressMessages(export_as_txt(lsting, lpp = 4, verbose = TRUE, page_break = "\n",
                                                 rep_cols = length(get_keycols(lsting))))
 
   testthat::expect_snapshot(cat(listing_exp))
+})
+
+
+testthat::test_that("key columns repeat with pagination with export_as_txt", {
+  # pre-processing and ordering
+  tbl <- as_listing(
+    iris,
+    key_cols = c("Species"),
+    disp_cols = c("Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width")
+  )
+
+  listing <- suppressMessages(export_as_txt(tbl, lpp = 20, paginate = TRUE,
+                                            page_break = "\n",
+                                            rep_cols = length(get_keycols(tbl))))
+
+
+  testthat::expect_snapshot(cat(listing))
 })
 
 testthat::test_that("Listing print correctly, with paginate", {
