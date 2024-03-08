eaaf5a0568573cfebde8ec26bc4abd087d363e3e
Emily de la Rua <emily.de_la_rua@contractors.roche.com>
2024-03-08 16:03:39 -0500

    Clean up paginate_listing documentation

--- R/paginate_listing.R
+++ R/paginate_listing.R
@@ -3,29 +3,23 @@
 #' @description `r lifecycle::badge("experimental")`
 #'
 #' Pagination of a listing. This can be vertical for long listings with many
-#' rows or horizontal if there are many columns.
+#' rows and/or horizontal if there are many columns.
 #'
-#' @param lsting listing_df. The listing to paginate.
-#' @param lpp numeric(1) or NULL. Number of row lines (not counting titles and
-#'   footers) to have per page. Standard is `70` while `NULL` disables vertical
-#'   pagination.
-#' @param cpp numeric(1) or NULL. Width (in characters) of the pages for
-#'   horizontal pagination. `NULL` (the default) indicates no horizontal
-#'   pagination should be done.
 #' @inheritParams formatters::pag_indices_inner
 #' @inheritParams formatters::vert_pag_indices
 #' @inheritParams formatters::page_lcpp
 #' @inheritParams formatters::toString
+#' @param lsting (`listing_df` or `list`)\cr the listing or list of listings to paginate.
+#' @param lpp (`numeric(1)` or `NULL`)\cr number of rows/lines (excluding titles and footers)
+#'   to include per page. Standard is `70` while `NULL` disables vertical pagination.
+#' @param cpp (`numeric(1)` or `NULL`)\cr width (in characters) of the pages for horizontal
+#'   pagination. `NULL` (the default) indicates no horizontal pagination should be done.
 #'
-#' @returns A list of listings' objects that are meant to be on separated pages.
-#'   For `pag_tt_indices` a list of paginated-groups of row-indices of `lsting`.
-#'
-#' @rdname paginate
+#' @return A list of `listing_df` objects where each list element corresponds to a separate page.
 #'
 #' @examples
 #' dat <- ex_adae
 #' lsting <- as_listing(dat[1:25, ], disp_cols = c("USUBJID", "AESOC", "RACE", "AETOXGR", "BMRKR1"))
-#'
 #' mat <- matrix_form(lsting)
 #'
 #' cat(toString(mat))
@@ -35,12 +29,9 @@
 #' paginate_listing(lsting, cpp = 100, lpp = 40)
 #'
 #' paginate_listing(lsting, cpp = 80, lpp = 40, verbose = TRUE)
-#' @export
 #'
-#' @return for `paginate_listing` a list containing separate
-#' `listing_df` objects for each page, for `pag_listing_indices`,
-#' a list of indices in the direction being paginated corresponding
-#' to the individual pages in that dimension.
+#' @export
+#' @rdname paginate
 paginate_listing <- function(lsting,
                              page_type = "letter",
                              font_family = "Courier",
@@ -125,14 +116,14 @@ paginate_listing <- function(lsting,
   ret
 }
 
-#' @title Defunct functions
+#' Defunct functions
 #'
-#' @description
-#' These functions are defunct and their symbols will be removed entirely
-#' in a future release.
-#' @rdname defunct
+#' These functions are defunct and their symbols will be removed entirely in a future release.
+#'
 #' @inheritParams paginate_listing
+#'
 #' @export
+#' @rdname defunct
 pag_listing_indices <- function(lsting,
                                 lpp = 15,
                                 colwidths = NULL,
