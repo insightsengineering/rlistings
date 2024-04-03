2957e3bce45ff52cf37540791142c579a6ab6024
Melkiades <davide.garolini@roche.com>
2024-04-03 15:00:38 +0200

    adding ::

--- tests/testthat/test-matrix_form.R
+++ tests/testthat/test-matrix_form.R
@@ -14,27 +14,27 @@ testthat::test_that("matrix_form keeps relevant information and structure about 
     keycols = c("fake_rownames", "Petal.Width"), add_decoration = FALSE
   )
 
-  expect_equal(names(mat_rebuilt), names(mat))
+  testthat::expect_equal(names(mat_rebuilt), names(mat))
 
   mat_rebuilt$row_info$pos_in_siblings <- mat$row_info$pos_in_siblings # not relevant in listings
   mat_rebuilt$row_info$n_siblings <- mat$row_info$n_siblings # not relevant in listings
-  expect_equal(mf_rinfo(mat_rebuilt), mf_rinfo(mat))
+  testthat::expect_equal(mf_rinfo(mat_rebuilt), mf_rinfo(mat))
 
   mat_rebuilt["page_titles"] <- list(NULL)
 
-  expect_equal(mat, mat_rebuilt)
+  testthat::expect_equal(mat, mat_rebuilt)
 
-  expect_equal(toString(mat), toString(mat_rebuilt))
+  testthat::expect_equal(toString(mat), toString(mat_rebuilt))
 
 
   # The same but with rownames
   lmf <- basic_listing_mf(mtcars)
-  expect_equal(ncol(lmf), length(lmf$col_widths))
-  expect_equal(ncol(lmf), ncol(lmf$strings))
-  expect_false(mf_has_rlabels(lmf))
+  testthat::expect_equal(ncol(lmf), length(lmf$col_widths))
+  testthat::expect_equal(ncol(lmf), ncol(lmf$strings))
+  testthat::expect_false(mf_has_rlabels(lmf))
 
   rlmf <- as_listing(mtcars) %>% matrix_form() # rownames are always ignored!!!
-  expect_equal(ncol(rlmf), length(rlmf$col_widths))
-  expect_equal(ncol(rlmf), ncol(rlmf$strings))
-  expect_false(mf_has_rlabels(rlmf))
+  testthat::expect_equal(ncol(rlmf), length(rlmf$col_widths))
+  testthat::expect_equal(ncol(rlmf), ncol(rlmf$strings))
+  testthat::expect_false(mf_has_rlabels(rlmf))
 })
